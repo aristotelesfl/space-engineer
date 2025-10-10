ef5506d1ba7ab166e898c7b7d70022fe02c4e905
Gabriel Albuquerque <fcogabrielmachadoalbuquerque@gmail.com>
2025-10-09 23:48:23 -0300

    Feat: Implementa mecânica de tiro e colisão da nave

--- src/game/index.tsx
+++ src/game/index.tsx
@@ -1,14 +1,19 @@
 // src/presentation/components/GameWrapper.tsx
 
 import React, { useEffect, useRef } from "react";
-import Game from "./Game";
+import Game, { handlePlayerShoot } from "../game/Game";
+import { useKeyboardHandler } from "../core/usecases/usekeyboardHandler";
 
 export default function GameWrapper() {
     const gameContainerRef = useRef<HTMLDivElement>(null);
 
+    useKeyboardHandler(handlePlayerShoot);
+
     useEffect(() => {
         if (gameContainerRef.current) {
-            Game.canvas?.parentNode?.removeChild(Game.canvas);
+            if (Game.canvas?.parentNode) {
+                Game.canvas.parentNode.removeChild(Game.canvas);
+            }
             gameContainerRef.current.appendChild(Game.canvas);
         }
     }, []);
@@ -21,6 +26,7 @@ export default function GameWrapper() {
                 maxWidth: "100vw",
                 height: "100vh",
                 backgroundColor: "#0d081dff",
+                cursor: "none",
             }}
         />
     );
